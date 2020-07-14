c985d1c5a0ce91806949318c8e9ff2acbb245ef8
Tova <tovi27818@gmail.com>
2020-07-14 08:24:07 +0300

    change environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,15 +9,12 @@ export const environment = {
   development: true,
 	
 	apiServer: {
-		port: 52718,
 		useHttps: false,
-		serverUrl: 'makeup4u.herokuapp.com',
+		serverUrl: 'makeup4userver.somee.com',
 		fullAddress: function() {
 			let protocol = environment.apiServer.useHttps ? 'https' : 'http';
 			let address = environment.apiServer.serverUrl;
-			let port = environment.apiServer.port && environment.apiServer.port > 0 ? `:${environment.apiServer.port}` : '';
-	
-			return `${protocol}://${address}${port}/`;
+			return `${protocol}://${address}/`;
 		}
 	},
 };
