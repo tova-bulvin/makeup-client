08e4b51f6f5c63baef960736d8dc98697b45d00c
Tova <tovi27818@gmail.com>
2020-08-06 10:09:57 +0300

    edit heder

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,10 +13,21 @@ declare var jQuery :any;
     @Input() linksInput:string;//string -list of boolean that indicate links that display in header. ex:"1,0,0,1"
     linksArray:string[];
 
+    ngOnInit(){
+      if(this.linksInput!=undefined){
+        this.displayLinks();
+      }
+    }
+
     ngAfterViewInit(){
+      if(this.linksInput!=undefined){
+        this.displayLinks();
+      }
+    }
+    displayLinks(){
       this.linksArray=this.linksInput.split(",");
       var i=0;
-      var links=jQuery(".links>a");
+      var links=jQuery(".links>a.link");
       this.linksArray.forEach((element) => {
         var cast=Boolean(Number(element));
         var display=(cast==true?"":"none");
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {TabMenuModule} from 'primeng/tabmenu';
-import { MenuItem } from 'primeng/api/menuitem';
 import {HeaderTopComponent} from "../header/header.component"
-
+declare var jQuery :any;
 
 @Component({
   selector: 'app-home',
@@ -10,13 +8,8 @@ import {HeaderTopComponent} from "../header/header.component"
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  items: MenuItem[];
   constructor() { }
 
   ngOnInit() {
-    this.items = [
-      {label: 'Admin', icon: 'pi pi-user', routerLink: ['/auto']},
-      {label: 'Shopping', icon: 'pi pi-shopping-cart', routerLink: ['/customer-main']}
-    ];
   }
 }
